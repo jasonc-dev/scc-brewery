7c05fc7fcf43f75c7627b720c46ec46182825351
jasonc-dev <jasoncohen07@gmail.com>
2022-03-07 17:34:44 +0000

    Enable CSRF

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -33,9 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
             .and()
             .formLogin().and()
-            .httpBasic()
-                .and()
-            .csrf().disable();
+            .httpBasic().and()
+            .csrf().ignoringAntMatchers("h2-console/**", "/api/**");
 
             // h2 console config
             http.headers().frameOptions().sameOrigin();
